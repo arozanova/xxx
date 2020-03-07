822d70fdd92e72f2e191b8e3faf62a0d02a9cb9b
arozanova <60844239+arozanova@users.noreply.github.com>
2020-03-07 10:05:22 +0000

    100%

--- MaxSliceSum.cc
+++ MaxSliceSum.cc
@@ -1,35 +1,32 @@
-// you can use includes, for example:
- #include <climits>
-
-// you can write to stdout for debugging purposes, e.g.
-// cout << "this is a debug message" << endl;
-
 int solution(vector<int> &A) {
-    int m=A[0];
+    int m = A[0];
     bool pos = false;
     
+    // check if we have only negative numbers
     for (auto a : A ){
         m = min(m,a);
         if (a >= 0) {
             pos = true;
         }
-
     }
-    int minmax=m ;
     
-    for (auto a : A ){
-        if (a < 0)
-            minmax = max(minmax,a);
-    }
-    if (!pos) 
+    // if we have no positive nubmers we return the maximum number
+    if (!pos) {
+        int minmax = m;
+        for (auto a : A ){
+            if (a < 0)
+                minmax = max(minmax, a);
+        }  
         return minmax;
-        
-    //cout << m ;
-    long  maxEnding  = 0;
-    long  maxSlice = maxEnding;
+    }
+
+    // as we have positive numbers, we proceed the usual way
+    int maxEnding = 0;
+    int maxSlice = maxEnding;
     for (size_t i = 0; i < A.size(); ++i) {
-        maxEnding = max(long(0), long(maxEnding+A[i]));
+        maxEnding = max(0, maxEnding+A[i]);
         maxSlice = max(maxSlice, maxEnding);
     }
-    return int(maxSlice);
+    
+    return maxSlice;
 }
