4c30366f34be87698fc4a7b522c30548c2c73322
arozanova <60844239+arozanova@users.noreply.github.com>
2023-04-10 14:45:35 +0100

    Update MaxDoubleSliceSum.cc
    100%

--- MaxDoubleSliceSum.cc
+++ MaxDoubleSliceSum.cc
@@ -1,50 +1,28 @@
+
 int solution(vector<int> &A) {
-    int m = A[0];
-    bool pos = false;
-    
-    // check if we have only negative numbers
-    for (auto a : A ){
-        m = min(m,a);
-        if (a >= 0) {
-            pos = true;
-        }
-    }
-    
-    // if we have no positive nubmers we return the maximum number
-    if (!pos) {
-        int minmax = m;
-        for (auto a : A ){
-            if (a < 0)
-                minmax = max(minmax, a);
-        }  
-        return minmax;
+    int n = A.size();
+    if (n==3) return 0;
+    vector<int> leftToRight ( A.size(), 0);
+    vector<int> rightToLeft ( A.size(), 0);
+
+    int current = 0;
+    for (size_t i = 1; i <A.size()-1; ++i) {
+        current  = max (0, current + A[i]);
+        leftToRight[i] = current;
     }
 
-    // as we have positive numbers, we proceed the usual way
-    int maxEnding = A[1];
-    int maxSlice = 0;
-    bool firstSliceDone = false;
-    int firstSlice = 0;
-    for (size_t i = 2; i < A.size(); ++i) {
-        if (maxEnding == 0) continue;
-        int maxEndingTemp = maxEnding;
-        maxEnding = max(0, maxEnding+A[i]);
-        
-        if (maxEnding == 0) {
-            if( firstSliceDone == false) {
-            firstSlice = maxEndingTemp;
-            firstSliceDone = true;
-            }
-            else {
-                maxSlice = max(maxSlice, firstSlice + maxEndingTemp);
-                firstSlice = 0;
-                firstSliceDone = false;
-            }
-        }
-        cout << "maxEnding " << maxEnding << " firstSlice " << firstSlice << " maxSlice "  << maxSlice << endl;
-        
-        
+
+    current = 0;
+    for (size_t i = A.size()-2; i >= 1; --i) { 
+        current  = max (0, current + A[i]);
+        rightToLeft[i] = current;
+
     }
-    
-    return maxSlice;
+
+
+    int m = 0;
+    for (size_t i = 1; i <A.size()-1; ++i){
+       m = max(leftToRight[i-1] + rightToLeft[i+1], m);
+    }
+    return m;
 }
